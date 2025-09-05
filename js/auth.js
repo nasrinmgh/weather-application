820e037e91d50cc9a95999323c2e80f3a41080b4
nasrinmgh <nasrinmohaghegh98@gmail.com>
2025-09-05 18:17:59 +0200

    folders structure updated-new folders/files/paths

--- js/auth.js
+++ js/auth.js
@@ -1,65 +1,82 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js';
+import { auth } from "/APIs/firebase-config";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 // TO SIGN UP
-const signUpForm = document.querySelector('.register-form form');
-signUpForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const successSignUp = await credentialCheck();
-    if (successSignUp) {
-        showWelcome();
-    }
-})
+const signUpForm = document.querySelector(".register-form form");
+signUpForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const successSignUp = await credentialCheck();
+  if (successSignUp) {
+    showWelcome();
+  }
+});
 
 async function credentialCheck() {
-    const email = document.getElementById('signEmail').value;
-    const pass = document.getElementById('signPassword').value;
+  const email = document.getElementById("signEmail").value;
+  const pass = document.getElementById("signPassword").value;
+  const passConfirm = document.getElementById("passConfirm").value;
+  const fullName = document.getElementById("fullName").value;
+  const agreeCheck = document.getElementById("agreeCheck");
 
-    try {
+  try {
     const userCredit = await createUserWithEmailAndPassword(auth, email, pass);
-     return true;
-    } catch(error) {
-        if (error.code === 'auth/weak-password') {
-            alert('Use stronger password');
-        } else if (error.code === 'auth/email-already-in-use') {
-            alert('This email is already in use');
-        } else {
-            alert('Sign up failed:' + error.message)
-        }
-        return false;
-    } 
+    return true;
+  } catch (error) {
+    if (error.code === "auth/weak-password") {
+      alert("Use stronger password");
+    } else if (error.code === "auth/email-already-in-use") {
+      alert("This email is already in use");
+    } else if (passConfirm !== pass) {
+      alert("Password confirm doesn not match");
+    } else if (agreeCheck !== checked) {
+      alert("Please agree with our terms");
+    } else if (!fullName) {
+      alert("Please enter your name");
+    } else {
+      alert("Sign up failed:" + error.message);
+    }
+    return false;
+  }
 }
 
 function showWelcome() {
-        const msgBox = document.createElement('div');
-        msgBox.innerHTML = 'Signed up successfully!';
-        const registerOption = document.querySelector('.register-option');
-        registerOption.append(msgBox);
+  const msgBox = document.createElement("div");
+  msgBox.innerHTML = "Signed up successfully!";
+  const registerOption = document.querySelector(".register-option");
+  registerOption.append(msgBox);
 
-setTimeout( () => {
-        msgBox.classList.add('msg-box');
-}, 1000);
+  setTimeout(() => {
+    msgBox.classList.add("msg-box");
+  }, 1000);
 }
 
 // TO LOGIN
-loginForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const loginEmail = document.getElementById('logEmail');
-    const loginPass = document.getElementById('logPassword');
-    try {
-        const loginUser = await signInWithEmailAndPassword(auth, loginEmail, loginPass);
-        if (loginUser) {
-            showWelcome();
-        }
-    } catch (error) {
-        if (error.code === 'auth/invalid-email') {
-            alert('Enter a vlid email');
-        } else if (error.code === 'auth/user-not-found') {
-            alert('No user exists with this email');
-        } else if (error.code === 'auth/wrong-password') {
-            alert('Incorrect password');
-        } else {
-            alert('Login failed:' + error.message);
-        }
-        return false;
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const loginEmail = document.getElementById("logEmail");
+  const loginPass = document.getElementById("logPassword");
+  try {
+    const loginUser = await signInWithEmailAndPassword(
+      auth,
+      loginEmail,
+      loginPass
+    );
+    if (loginUser) {
+      showWelcome();
     }
-})
+  } catch (error) {
+    if (error.code === "auth/invalid-email") {
+      alert("Enter a vlid email");
+    } else if (error.code === "auth/user-not-found") {
+      alert("No user exists with this email");
+    } else if (error.code === "auth/wrong-password") {
+      alert("Incorrect password");
+    } else {
+      alert("Login failed:" + error.message);
+    }
+    return false;
+  }
+});
